9338251ed98c1066218ce6f08bbef09494d9c54d
Nuria.ricart <nuria.rs96@gmail.com>
2017-10-06 16:12:52 +0200

    2) Botó suma i resta funcionen

--- app/src/main/java/edu/upc/eseiaat/pma/simplecounter/SimpleCounterActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/simplecounter/SimpleCounterActivity.java
@@ -2,12 +2,44 @@ package edu.upc.eseiaat.pma.simplecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SimpleCounterActivity extends AppCompatActivity {
+
+    private int num;
+    private TextView label_num;
+    private String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_counter);
+
+        Button btn_plus = (Button) findViewById(R.id.btn_plus);
+        Button btn_resta = (Button) findViewById(R.id.btn_resta);
+        label_num = (TextView) findViewById(R.id.label_num);
+        num = 0;
+        label_num.setText("0");
+
+
+        btn_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num++;
+                number = Integer.toString(num);
+                label_num.setText(number);
+            }
+        });
+
+        btn_resta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num--;
+                number = Integer.toString(num);
+                label_num.setText(number);
+            }
+        });
     }
 }
